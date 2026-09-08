95fc6487da0c07e3de750e7ab00390c67dd5498f
Andre2117-maker <andrehenriq2005@gmail.com>
2026-09-07 23:53:28 -0400

    Ajusta base path para ./ no vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/simulador-sinais-sistemas/", // <-- Coloque o nome exato do seu repositório aqui
+  base: "./",
 });
